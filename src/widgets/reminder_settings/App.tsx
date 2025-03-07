e628d3fac8eaeaf1f2e3c64fe5934659a666b9ac
Konstantin Mis <konstantin.mis@twenty20.de>
2025-03-07 14:34:35 +0100

    removed log

--- src/widgets/reminder_settings/App.tsx
+++ src/widgets/reminder_settings/App.tsx
@@ -62,8 +62,6 @@ export default function App() {
             }
         }
 
-        console.log(filteredReminders);
-
         setReminders(filteredReminders);
         setIsLoading(false);
     };
